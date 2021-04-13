39b01e9a5b00c0096652cde7a83af8b2adbda881
Marcelo Adrian Cordero <marceloadriancordero@gmail.com>
2021-04-13 13:47:58 +0200

    Rutas configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 import { MainComponent } from './home/main/main.component';
-import { SharedModule } from './shared/shared.module';
 import { ServerUrlInterceptor } from './interceptors/server-url.interceptor';
-import { ProjectsModule } from './projects/projects.module';
 import { NewTeamComponent } from './teams/components/new-team/new-team.component';
-
+import { IndexComponent } from './home/index/index.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    NewTeamComponent
+    NewTeamComponent,
+    IndexComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    UsersModule,
-    AuthModule,
-    SharedModule,
-    ProjectsModule
+    AppRoutingModule,
+    HttpClientModule,    
   ],
   providers: [
     {
