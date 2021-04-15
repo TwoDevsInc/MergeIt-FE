0570868b9a3a7feb36680ef8c9fc8dd81c6975c9
Drakon <33479186+marinnx@users.noreply.github.com>
2021-04-15 19:11:46 +0200

    errores everywhere

--- src/app/projects/components/add-project/add-project.component.ts
+++ src/app/projects/components/add-project/add-project.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Team } from 'src/app/teams/interfaces/team.interface';
+import { Project } from '../../interfaces/project.interface';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'add-project',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-project.component.css']
 })
 export class AddProjectComponent implements OnInit {
-
-  constructor() { }
+  @Input() team!: Team;
+  aux: number = 0;
+  project: Project ={
+    name: '',
+    team: undefined,
+    taskLists: []
+  }
+  constructor(public activeModal: NgbActiveModal, private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    console.log(this.team);
+  }
+
+  addProject() {
+    console.log(this.project.name)
+    if (this.project.name !== ''){
+      console.log(`he llegadoooooo`)
+      this.aux = this.team.id!;
+      const team = { id : this.aux, name: "asdsa", users : [], projects : []};
+      this.projectService.addProjectToTeam(this.project,team, 50);
+    }
   }
 
 }
